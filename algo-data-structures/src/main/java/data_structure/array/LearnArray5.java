07b073557c4d4413a037d690f0e5a5805cca43b3
chen tong <chen2015tong@gmail.com>
2024-11-27 20:11:22 +0100

    update First Missing Positive

--- algo-data-structures/src/main/java/data_structure/array/LearnArray5.java
+++ algo-data-structures/src/main/java/data_structure/array/LearnArray5.java
@@ -55,7 +55,7 @@ public class LearnArray5 {
         return maxScore;
     }
 
-    // TODO: 数组金典案列, 利用数组空间"暂存数据", 使用3轮遍历 !!
+    // TODO: 数组金典案列: 数组的Index坐标就是连接的正整数，判断正整数值是否在该在的Index位置
     // First Missing Positive
     // Given an unsorted integer array nums, find the smallest missing positive integer
     // You must implement an algorithm that
@@ -63,72 +63,33 @@ public class LearnArray5 {
     // - Uses O(1) constant extra space
     //
     // nums = [1,2,0] -> 3
-    // nums = [3,4,-1,1] -> 2
-    // -1 4 3 1
-    // 1 4 3 -1
-    //
+    // nums = [3,4,-1,1] -> 2    必须考虑负数的情况
     // nums = [7,8,9,11,12] -> 1 从最小的正数开始考虑
-    //
-    // 利用数组本身来完成坐标的转换
     public int firstMissingPositive(int[] nums) {
+        // 第一遍调整值的位置，交换移动到index位置
+        for (int index=0; index < nums.length; index++) {
+            int value = nums[index];
 
-        int firstPositive = 1;
-        for (int index=0; index<nums.length; index++) {
-
-        }
-
-
-
-
-        return 1;
-    }
-
-
-
-
-    public int firstMissingPositive1(int[] nums) {
-        // 测试理解：1. 使用HashSet<>存储数据，然后从1开始判断缺失位置  O(n), O(n)
-        HashSet<Integer> set = new HashSet<>();
-        for (int num : nums) set.add(num);
-        for (int index = 1; index <= nums.length; index++) {
-            if (!set.contains(index)) {
-                return index;
+            // int absValue = Math.abs(a[i])
+            // a[absValue - 1] == a[i] ?
+            if (value < 1 || value > nums.length || value == index + 1) {
+                continue;
             }
-        }
-        return nums.length + 1; // 如果前面从数字1开始依次排序，则第一缺失的最小数是length+1
-    }
 
-    // 正确理解：1. > 理论基础1: [1,n]数组，如果数字全部从1到n连续出现，则第一个缺失的正数必然是n+1     ===> 时间复杂度比上面减低了15倍以上
-    //            > 理论基础2: 如果数字区间有位置缺失，则必然在[1,n]区间中，排除负数和超区间外的值     ===> 稳定的算法
-    // 6 0 9 2 3 1  -1  ==> 第一轮将边界外的值设置成特殊的值
-    // 6 0 8 2 3 1  8   ==> 将负数和大数统一设置成n+1特征值，避免使用到该index来定位(因为在数组有效的范围之外)  !!
-    // 6 0 8 2 3 -1 8   ==> 每个位置的值，使用绝对值来确定要标记的位置，这里的-1在被标记完之后，还能再次使用来定位 !!
-    //           x
-    //   x
-    //     x
-    // x
-    // x x x     x      ==> 标记完成的结果，找到每一标记的位置，就是第一个缺失的正数
-    public int firstMissingPositive2(int[] nums) {
-        int n = nums.length;
-        for (int index = 0; index < n; index++) {
-            if (nums[index] <= 0 || nums[index] > n) {
-                nums[index] = n + 1; // n+1 特征的边界值
-            }
+            int position = value - 1;
+            int temp = nums[position];
+            nums[position] = value;
+            nums[index] = temp;
+            // Move back forwards of index
+            index--;
         }
-        for (int index = 0; index < n; index++) {
-            int position = Math.abs(nums[index]);
-            if (position <= n) {
-                position--;          // 确定要"标记"负值的坐标位置; 如果要标记的位置上是负数，说明被标记过，无需再标记 !!
-                if (nums[position] > 0) {
-                    nums[position] = -1 * nums[position];
-                }
-            }
-        }
-        for (int index = 0; index < n; index++) {
-            if (nums[index] > 0) {
+
+        // 第二遍值和index位置不匹配的点
+        for (int index=0; index < nums.length; index++) {
+            if (index != nums[index] - 1) {
                 return index + 1;
             }
         }
-        return n + 1;
+        return nums.length + 1;
     }
 }
