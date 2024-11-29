c4e72f6efeeff76ef5222b2d70df186f716f68b5
chen tong <chen2015tong@gmail.com>
2024-11-29 11:45:50 +0100

    add t9 model mapping question

--- algo-interviews/src/main/java/criteo/SlidingWindowMax.java
+++ algo-interviews/src/main/java/criteo/SlidingWindowMax.java
@@ -3,57 +3,7 @@ package criteo;
 import java.util.ArrayList;
 import java.util.List;
 
-// 有关Sliding Windows滑动窗口算法的案例运用
-public class SlidingWindowQuestions {
-
-    public static void main(String[] args) {
-        int[] nums = {2, 5, 3, 4, 1, 8};
-        List<Integer> result = getSlidingMax(nums, 2);
-        for (int item: result) {
-            System.out.println(item);
-        }
-    }
-
-    // TODO. 滑动过程中如果窗口的Size确定，则可以只使用一个Index
-    // Sliding Average
-    // Calculate the average in windows size k, return list of averages
-    //
-    // input = 2 5 3 1 8, k = 3
-    // output = 3, 3, 4
-    //
-    // O(n)     时间基于遍历完所有数据
-    // O(n-k+1) 空间基于会产生多少结果，最糟糕是O(n), 最佳为O(1)
-    public static List<Integer> getSlidingAverage(int[] nums, int k) {
-        List<Integer> averageList = new ArrayList<>();
-        // For special cases and parameters
-        if (nums == null || nums.length < k) {
-            return averageList;
-        }
-        if (k == 1) {
-            for (int num: nums) {
-                averageList.add(num);
-            }
-            return averageList;
-        }
-
-        // For the first k numbers average
-        int index = 0;
-        int sum = 0;
-        while (index < k) {
-            sum += nums[index];
-            index++;
-        }
-        averageList.add(sum / k);
-
-        // For remaining of the numbers
-        while (index < nums.length) {
-            sum += nums[index];
-            sum -= nums[index - k]; // remove the first num
-            averageList.add(sum / k);
-            index++;
-        }
-        return averageList;
-    }
+public class SlidingWindowMax {
 
     // TODO. 使用一个Index来标识在Window中要找的结果
     // Sliding Max
@@ -98,5 +48,13 @@ public class SlidingWindowQuestions {
             rightIndex++;
         }
         return maxList;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 5, 3, 4, 1, 8};
+        List<Integer> result = getSlidingMax(nums, 2);
+        for (int item: result) {
+            System.out.println(item);
+        }
     }
 }
---
+++ algo-interviews/src/main/java/criteo/T9ModelMapping.java
@@ -0,0 +1,65 @@
+package criteo;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class T9ModelMapping {
+
+    public static void main(String[] args) {
+        List<String> dict = List.of("sage", "rage", "raid", "hello", "morning", "blabla");
+        List<String> result = getMappingStringsOfDigits( dict, "7243");
+        for (String str: result) {
+            System.out.println(str);
+        }
+    }
+
+    // TODO. 时间复杂度分步计算 + 空间复杂度的优化
+    //  HashMap中不能存储dict中str值，这会导致空间复杂度double增加
+    //  HashMap中只需存储dict中的字符串的坐标，根据坐标直接取结果
+    private static List<String> getMappingStringsOfDigits(List<String> dict, String digits) {
+        HashMap<String, List<Integer>> map = new HashMap<>();
+        for(int index = 0; index < dict.size(); index++) {
+            StringBuilder stringBuilder = new StringBuilder();
+            String value = dict.get(index);
+            for(char c: value.toCharArray()) {
+                stringBuilder.append(getDigitByChar(c));
+            }
+
+            String key = stringBuilder.toString();
+            if(map.containsKey(key)) {
+                List<Integer> list = map.get(key);
+                list.add(index);
+            } else {
+                List<Integer> newList = new ArrayList<>();
+                newList.add(index);
+                map.put(key, newList);
+            }
+        }
+
+        List<String> result = new ArrayList<>();
+        if (map.containsKey(digits)) {
+            for (int index: map.get(digits)) {
+                result.add(dict.get(index));
+            }
+        }
+        return result;
+    }
+
+    // 以下的Mapping方法不造成时间复杂度
+    private static int getDigitByChar(char character) {
+        return switch (character) {
+            case 'a', 'b', 'c' -> 2;
+            case 'd', 'e', 'f' -> 3;
+            case 'g', 'h', 'i' -> 4;
+            case 'j', 'k', 'l' -> 5;
+            case 'm', 'n', 'o' -> 6;
+            case 'p', 'q', 'r', 's' -> 7;
+            case 't', 'u', 'v' -> 8;
+            case 'w', 'x', 'y', 'z' -> 9;
+            case ' ' -> 0;
+            default -> -1;
+        };
+    }
+}
