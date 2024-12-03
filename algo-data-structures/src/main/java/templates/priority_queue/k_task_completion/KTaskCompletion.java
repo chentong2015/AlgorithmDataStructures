57b471055fa82ce4036e0842297cfe08964fda6e
chen tong <chen2015tong@gmail.com>
2024-12-03 17:22:46 +0100

    finish priority queue k task question

--- algo-data-structures/src/main/java/templates/priority_queue/k_task_completion/KTaskCompletion.java
+++ algo-data-structures/src/main/java/templates/priority_queue/k_task_completion/KTaskCompletion.java
@@ -5,21 +5,26 @@ import java.util.*;
 public class KTaskCompletion {
 
     // TODO. 优先级队列用于存储TopK最大差异的收益
+    // O(n*log(k)) 每次遍历都可能导致优先级队列的调整
+    // O(k)        优先级队列只占k级别的复杂度
     public static int getMaximumRewardPoints(int k, List<Integer> reward1, List<Integer> reward2) {
         PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
         int maxRewardPoints = 0;
         for (int index=0; index < reward1.size(); index++) {
             maxRewardPoints += reward2.get(index);
+
+            // 将优先级队列中多余的最小的数据移除
             priorityQueue.add(reward1.get(index - reward2.get(index)));
+            if (priorityQueue.size() > k) {
+                priorityQueue.poll();
+            }
         }
 
-
-        return 0;
+        while (!priorityQueue.isEmpty()) {
+            maxRewardPoints += priorityQueue.poll();
+        }
+        return maxRewardPoints;
     }
-
-
-
-
 
     // TODO. 一般逻辑的算法复杂度太差
     // O(n*logn) 排序算法复杂度
