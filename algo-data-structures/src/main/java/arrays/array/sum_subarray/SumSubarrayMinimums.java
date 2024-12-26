50b8af2b2c653be6b64d505bc626ff7d7971609e
chen tong <chen2015tong@gmail.com>
2024-12-26 18:57:08 +0100

    update sum subarray min questions

--- algo-data-structures/src/main/java/arrays/array/sum_subarray/SumSubarrayMinimums.java
+++ algo-data-structures/src/main/java/arrays/array/sum_subarray/SumSubarrayMinimums.java
@@ -15,35 +15,43 @@ public class SumSubarrayMinimums {
 
     public static void main(String[] args) {
         int[] arr = {3,1,2,4};
-        System.out.println(sumSubarrayMins(arr));
+        int[] nums = {11,81,94,43,3};
+        System.out.println(sumSubarrayMins(nums));
     }
 
-    // TODO. 使用Stack[]数组存储Index位置前面所有它小的坐标
-    //  在Stack添加和取值的时候，通过判断来计算最后的结果
-    //
-    // 3  1  2     4      7         3
-    //    1  1+1'  2+1+1' 4+2+1+1'  3+3+2+1+1'
-    //
+    // TODO. Stack[]数组存储Index位置前面所有更小值的坐标
+    //  [first ... middle ... right .. Index] 记录的之前坐标需要分三段计算
+    // 3  1  2  4 -
+    // 3 0 0 0
+    // 1 0 0 0
+    // 1 2 0 0
+    // 1 2 4 0
     public static int sumSubarrayMins(int[] arr) {
         int sumMin = 0;
-        int countIndexBefore = 0;
-        int[] stack = new int[arr.length];
+        int indexMin = -1;
+        int[] stackMinBefore = new int[arr.length];
         for (int index=0; index < arr.length; index++) {
             sumMin += arr[index];
-
-            while (countIndexBefore > 0 && arr[stack[countIndexBefore - 1]] > arr[index]) {
-                sumMin += arr[index];
-                countIndexBefore--;
+            while (indexMin > -1 && arr[stackMinBefore[indexMin]] >= arr[index]) {
+                indexMin--;
             }
-            for (int j = 0; j < countIndexBefore; j++) {
-                sumMin += arr[stack[j]];
-                if (j==0) {
-                    sumMin += arr[stack[j]] * stack[j]; // 最小值之前的统计
+
+            if (indexMin == -1) {
+                sumMin += arr[index] * index;
+            } else {
+                int lastIndex = stackMinBefore[indexMin];
+                sumMin += arr[lastIndex] * (index - lastIndex);
+
+                for (int middle = indexMin; middle >= 1; middle--) {
+                    int rightIndex = stackMinBefore[middle];
+                    int leftIndex = stackMinBefore[middle -1];
+                    sumMin += arr[leftIndex] * (rightIndex - leftIndex);
                 }
-            }
 
-            stack[countIndexBefore] = index;
-            countIndexBefore++;
+                int firstIndex = stackMinBefore[0];
+                sumMin += arr[firstIndex] * (firstIndex);
+            }
+            stackMinBefore[++indexMin] = index;
         }
         return sumMin;
     }
