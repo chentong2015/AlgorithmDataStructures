d424f7bfd69f782e9ae0484a79af33d6b8edd1be
chen tong <chen2015tong@gmail.com>
2025-01-01 15:44:08 +0100

    add max num sliding window budget problem

--- interview-cases/src/main/java/criteo/max_sliding_window/SlidingWindowMax.java
+++ interview-cases/src/main/java/criteo/max_sliding_window/SlidingWindowMax.java
@@ -2,38 +2,45 @@ package criteo.max_sliding_window;
 
 import java.util.LinkedList;
 
+// Sliding Window Maximum
+// 使用额外的链表存储，以空间换取最佳的时间复杂度
 public class SlidingWindowMax {
 
     public static void main(String[] args) {
-        int[] nums = {2, 5, 3, 4, 1, 8};
-        int[] nums2 = {1, 3, -1, -3, 5, 3, 6, 7};
-
-        int[] results = maxSlidingWindow(nums2, 3);
+        int[] nums = {1, 3, -1, -3, 5, 3, 6, 7};
+        int[] results = maxSlidingWindow(nums, 3);
         for (int item: results) {
             System.out.println(item);
         }
     }
 
     // TODO. 数据压缩思想: 将遍历的数据压缩到LinkedList<>存储空间，用于计算结果
-    //  链表存储数据坐标位置，减少存储空间
-    //  链表存储时剔除无用的前面小数据，减少存储的长度
+    //  - 链表存储数据Index坐标位置，减少存储空间
+    //  - 链表存储时剔除无用的前面小数据，减少存储长度
+    //  - LinkedList链表头部存储的窗口中最大值位置
     public static int[] maxSlidingWindow(int[] nums, int k) {
         if (nums.length == 0) {
             return nums;
         }
         int[] result = new int[nums.length - k + 1];
-        LinkedList<Integer> dq = new LinkedList<>();
+        LinkedList<Integer> linkedList = new LinkedList<>();
         for (int i = 0; i < nums.length; i++) {
-            if (!dq.isEmpty() && dq.peek() < i - k + 1) {
-                dq.poll(); // 移除Head 窗口之外的坐标数据
-            }
-            while (!dq.isEmpty() && nums[dq.peekLast()] <= nums[i]) {
-                dq.pollLast(); // 将前面的比当前位置小的数据移除
+            // 移除窗口之外的坐标数据: 窗口的宽度定长
+            if (!linkedList.isEmpty() && linkedList.peek() < i - k + 1) {
+                linkedList.poll();
             }
 
-            dq.offer(i); // 加入当前的位置，LinkedList中前面有更大的数据或者没有
+            // 将前面的比当前位置小的数据移除: 注意这里比较值，而非比较坐标
+            while (!linkedList.isEmpty() && nums[linkedList.peekLast()] <= nums[i]) {
+                linkedList.pollLast();
+            }
+
+            // 加入当前的位置: Index前面有更大的数据或者没有
+            linkedList.offer(i);
+
             if (i - k + 1 >= 0) {
-                result[i - k + 1] = nums[dq.peek()]; // 取Head 位置坐标数据
+                int headIndex = linkedList.peek();
+                result[i - k + 1] = nums[headIndex];
             }
         }
         return result;
