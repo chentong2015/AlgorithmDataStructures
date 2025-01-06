e25d324a9365155adb282090f4b7282940cb34ff
chen tong <chen2015tong@gmail.com>
2025-01-06 17:59:11 +0100

    complete sum subarray mininum question

--- data-structures/src/main/java/arrays/subarrays/SumSubarrayMin.java
+++ data-structures/src/main/java/arrays/subarrays/SumSubarrayMin.java
@@ -6,53 +6,59 @@ package arrays.subarrays;
 // Since the answer may be large, return the answer modulo 10^9 + 7.
 //
 // arr = [3,1,2,4] -> Output: 17
-// Subarrays [3], [1], [2], [4], [3,1], [1,2], [2,4], [3,1,2], [1,2,4], [3,1,2,4].
-// Minimums  3, 1, 2, 4, 1, 1, 2, 1, 1, 1.
+// Subarrays
+// [3], [1], [2], [4],
+// [3,1], [1,2], [2,4],
+// [3,1,2], [1,2,4],
+// [3,1,2,4].
+// 3, 1, 2, 4, 1, 1, 2, 1, 1, 1.
 //
 // 1 <= arr.length <= 3 * 10^4
 // 1 <= arr[i] <= 3 * 10^4
 public class SumSubarrayMin {
 
-    public static void main(String[] args) {
-        int[] arr = {3,1,2,4};
-        int[] nums = {11,81,94,43,3};
-        System.out.println(sumSubarrayMins(nums));
-    }
+    // TODO. 利用Monotonic Stack单调栈来存储遍历过程中有用的坐标
+    // Input: 3  1  2  4
+    // Stack:
+    //          4 存储值
+    //       2  2
+    // 3  1  1  1
+    //    -  -  -
+    //
+    //          3 存储坐标: 用于计算Gap间隔
+    //       2  2
+    // 0  1  1  1
+    //
+    // O(n ~ n*n) 每读取一个新元素就会遍历栈
+    // O(n)       栈中可能存储所有的坐标
+    public static int sumSubarrayMins(int[] nums) {
+        long sum = 0;
+        int count = 0;
+        int[] stack = new int[nums.length];
 
-    // TODO. Stack[]数组存储Index位置前面所有更小值的坐标
-    //  [first ... middle ... right .. Index] 记录的之前坐标需要分三段计算
-    // 3  1  2  4 -
-    // 3 0 0 0
-    // 1 0 0 0
-    // 1 2 0 0
-    // 1 2 4 0
-    public static int sumSubarrayMins(int[] arr) {
-        int sumMin = 0;
-        int indexMin = -1;
-        int[] stackMinBefore = new int[arr.length];
-        for (int index=0; index < arr.length; index++) {
-            sumMin += arr[index];
-            while (indexMin > -1 && arr[stackMinBefore[indexMin]] >= arr[index]) {
-                indexMin--;
+        for (int index=0; index < nums.length; index++) {
+            // 先将Stack栈中更大值的index坐标移除
+            while (count > 0 && nums[stack[count -1]] > nums[index]) {
+                stack[count - 1] = 0;
+                count--;
             }
 
-            if (indexMin == -1) {
-                sumMin += arr[index] * index;
-            } else {
-                int lastIndex = stackMinBefore[indexMin];
-                sumMin += arr[lastIndex] * (index - lastIndex);
+            // 将新坐标押入栈中(最后栈顶的位置)
+            count++;
+            stack[count - 1] = index;
 
-                for (int middle = indexMin; middle >= 1; middle--) {
-                    int rightIndex = stackMinBefore[middle];
-                    int leftIndex = stackMinBefore[middle -1];
-                    sumMin += arr[leftIndex] * (rightIndex - leftIndex);
-                }
-
-                int firstIndex = stackMinBefore[0];
-                sumMin += arr[firstIndex] * (firstIndex);
+            // TODO. 根据栈中两个值之间“Gap间隔”来计算结果
+            for (int right = count - 1; right > 0; right--) {
+                int value = nums[stack[right]];
+                int indexGap = stack[right] - stack[right -1];
+                sum += value * indexGap;
             }
-            stackMinBefore[++indexMin] = index;
+
+            // TODO. 最后统计Stack栈底元素的统计
+            int value = nums[stack[0]];
+            sum += value * (stack[0] + 1);
         }
-        return sumMin;
+        int mod = (int) 1e9 + 7;
+        return (int) (sum % mod);
     }
 }
