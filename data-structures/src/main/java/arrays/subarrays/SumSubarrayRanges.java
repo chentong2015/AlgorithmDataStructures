d949d8156bf6c651877a52521f98b0d175e4c667
chen tong <chen2015tong@gmail.com>
2025-01-07 10:21:41 +0100

    refresh lp principles

--- data-structures/src/main/java/arrays/subarrays/SumSubarrayRanges.java
+++ data-structures/src/main/java/arrays/subarrays/SumSubarrayRanges.java
@@ -20,17 +20,31 @@ package arrays.subarrays;
 public class SumSubarrayRanges {
 
     // TODO. DP + Stack 暂存读取过的“有用特征数据“
-    public static void main(String[] args) {
-        int[] nums = {1, 2, 3, 4};
-        int[] nums1 = {4, -2, -3, 4, 1};
-        System.out.println(subArrayRanges(nums1));
-    }
-
+    // 1   3   3
+    // -1  0   0  = min id before
+    // -1  -1  -1 = max id before
+    // 0 + 2 + 2  = 4
+    //
+    // 1   2   3
+    // -1  0   1  = min id before
+    // -1  -1  -1 = max id before
+    // 0 + 1 + 1+2 = 4
+    //
+    // 4,  -2,  -3,  4,   1
+    // -1  -1   -1   2    2 = min id before
+    // -1  0    1    -1   3 = max id before
+    // 0 + 6+ 1+7+ 7+6+ 3+4+3+3
     public static long subArrayRanges(int[] nums) {
         if (nums.length == 1) {
             return 0;
         }
 
         return 0;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4};
+        int[] nums1 = {4, -2, -3, 4, 1};
+        System.out.println(subArrayRanges(nums1));
     }
 }
