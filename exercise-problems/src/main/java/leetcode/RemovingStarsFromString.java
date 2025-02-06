9d9f25b180c3ea04b4e4c035aad1f888e38c99ea
tchen <chen2015tong@gmail.com>
2025-02-06 15:45:55 +0100

    add empty question

---
+++ exercise-problems/src/main/java/leetcode/RemovingStarsFromString.java
@@ -0,0 +1,31 @@
+package leetcode;
+
+// Removing Stars From a String
+// You are given a string s, which contains stars *.
+// In one operation, you can:
+// - Choose a star in s.
+// - Remove the closest non-star character to its left, as well as remove the star itself.
+// Return the string after all stars have been removed.
+//
+// 1 <= s.length <= 10^¨5
+// s consists of lowercase English letters and stars *.
+// The operation above can be performed on s.
+//
+// Note:
+// The input will be generated such that the operation is always possible.
+// It can be shown that the resulting string will always be unique.
+public class RemovingStarsFromString {
+
+    // TODO. To check
+    // leet**cod*e
+    // lee*cod*e
+    // lecod*e
+    // lecoe
+    //
+    // erase*****
+    public String removeStars(String s) {
+
+
+        return "";
+    }
+}
