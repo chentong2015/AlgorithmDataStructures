247ccb32c1ed9071a33223a1c59dbce8a2dbffc9
chen tong <chen2015tong@gmail.com>
2025-02-26 10:53:20 +0100

    add integer to roman question

--- interview-questions/src/main/java/amazon/interview/integer_to_english/IntegerToEnglishWord.java
+++ interview-questions/src/main/java/amazon/interview/integer_to_english/IntegerToEnglishWord.java
@@ -1,9 +1,9 @@
-package amazon.interview.number_to_english;
+package amazon.interview.integer_to_english;
 
-// TODO. 本质上属于recursion递归 + mapping映射算法
+// TODO. Recursion递归算法, 取不同位置上的值来匹配映射
 // O(1) 递归会的时间复杂度是Linear线性的，只和数字的个数有关
 // O(1) 空间复杂度也是线性的，使用常量的内存空间
-public class NumWordsConversion {
+public class IntegerToEnglishWord {
 
     private final String[] tens = {"", "", "Twenty", "Thirty", "Forty",
             "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
@@ -14,26 +14,23 @@ public class NumWordsConversion {
             "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
 
     public String numberToWords(int num) {
-        if (num == 0) {
-            return "Zero";
-        }
-        return helper(num);
+        return num == 0 ? "Zero": convert(num);
     }
 
-    private String helper(int num) {
+    private String convert(int num) {
         StringBuilder result = new StringBuilder();
         if (num < 20) {
             result.append(belowTwenty[num]);
         } else if (num < 100) {
             result.append(tens[num / 10]).append(" ").append(belowTwenty[num % 10]);
         } else if (num < 1000) {
-            result.append(helper(num / 100)).append(" Hundred ").append(helper(num % 100));
+            result.append(convert(num / 100)).append(" Hundred ").append(convert(num % 100));
         } else if (num < 1000000) {
-            result.append(helper(num / 1000)).append(" Thousand ").append(helper(num % 1000));
+            result.append(convert(num / 1000)).append(" Thousand ").append(convert(num % 1000));
         } else if (num < 1000000000) {
-            result.append(helper(num / 1000000)).append(" Million ").append(helper(num % 1000000));
+            result.append(convert(num / 1000000)).append(" Million ").append(convert(num % 1000000));
         } else {
-            result.append(helper(num / 1000000000)).append(" Billion ").append(helper(num % 1000000000));
+            result.append(convert(num / 1000000000)).append(" Billion ").append(convert(num % 1000000000));
         }
         return result.toString().trim();
     }
