a9ee44dc5e754af76a29c4a77ea10ca510095991
chen tong <chen2015tong@gmail.com>
2025-03-10 19:16:43 +0100

    update Smallest String With Swaps question

--- exercise-problems/src/main/java/leetcode2/SmallestStringWithSwaps.java
+++ exercise-problems/src/main/java/leetcode2/SmallestStringWithSwaps.java
@@ -1,6 +1,6 @@
 package leetcode2;
 
-import java.util.List;
+import java.util.*;
 
 // Smallest String With Swaps
 // You are given a string s and an array of pairs of indices in the string pairs
@@ -14,7 +14,8 @@ import java.util.List;
 // s only contains lower case English letters.
 public class SmallestStringWithSwaps {
 
-    // TODO. 不要考虑中间交换过程的数据，直接判断最终的结果
+    // TODO. 将Pair坐标位置进行分组，同组的字符串取最小值，最后拼接片段
+    //
     // s = "dcab", pairs = [[0,3],[1,2]]
     // swap s[0] and s[3], s = "bcad"
     // swap s[1] and s[2], s = "bacd"
@@ -29,13 +30,61 @@ public class SmallestStringWithSwaps {
     // swap s[1] and s[2], s = "bac"
     // swap s[0] and s[1], s = "abc"
     //
-    // 如果交换的位置能够联通，则返回最小的重组字符串
-    // 如果无法联通所有位置，则拼接所有最小字符串片段
-    // 0 1 2 3
-    // 0 1 2 3
-    // 0 1 2
+    // TODO. 一旦构成Group之后，组内的所有的字符取最小结果
+    // 不同的组之间需要链接合并
+    // 0 1 2 3 4 5
+    // 0 -> {0, 3, 4, 2}
+    // 1 -> {1, 5}
+    //
     public String smallestStringWithSwaps(String s, List<List<Integer>> pairs) {
+        int[] root = new int[s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            root[i] = i;
+        }
 
+        // Union pairs
+        for (List<Integer> pair : pairs) {
+            union( pair.get(0),  pair.get(1), root);
+        }
 
+        // Group indices by their root parent
+        Map<Integer, List<Integer>> groups = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            int rootI = find(i, root);
+            groups.computeIfAbsent(rootI, k -> new ArrayList<>()).add(i);
+        }
+
+        // Sort characters within each group
+        char[] result = s.toCharArray();
+        for (List<Integer> indices : groups.values()) {
+            List<Character> chars = new ArrayList<>();
+            for (int index : indices) {
+                chars.add(s.charAt(index));
+            }
+            Collections.sort(chars);
+            Collections.sort(indices);
+
+            // Place sorted characters back into the result array
+            for (int i = 0; i < indices.size(); i++) {
+                result[indices.get(i)] = chars.get(i);
+            }
+        }
+        return new String(result);
+    }
+
+    // 将Pair坐标进行联合，构成一个Group组
+    private void union(int x, int y, int[] root) {
+        int rootX = find(x, root);
+        int rootY = find(y, root);
+        if (rootX != rootY) {
+            root[rootY] = rootX;
+        }
+    }
+
+    private int find(int x, int[] root) {
+        if (x == root[x]) {
+            return x;
+        }
+        return root[x] = find(root[x], root);
     }
 }
