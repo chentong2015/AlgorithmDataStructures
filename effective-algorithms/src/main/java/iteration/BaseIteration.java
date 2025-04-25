0118456da7aef4187c5bad2e0a99fc705f6954c5
tchen <chen2015tong@gmail.com>
2025-04-25 17:03:32 +0200

    update base iteration

--- effective-algorithms/src/main/java/iteration/BaseIteration.java
+++ effective-algorithms/src/main/java/iteration/BaseIteration.java
@@ -1,9 +1,11 @@
 package iteration;
 
-// Fibonacci数列 Fib(n)=Fib(n−1)+Fib(n−2)
 public class BaseIteration {
 
-    // TODO: 借助动态编程思想进行迭代
+
+
+
+    // TODO: 在循环的过程中迭代前面计算过的值
     // 第一步有一种可能，
     // 第二步有两种可能，
     // 第三步是前两种情况的和(从第一步和第二步都能够走到第三步)
@@ -19,6 +21,25 @@ public class BaseIteration {
             int third = first + second;
             first = second;
             second = third;
+        }
+        return second;
+    }
+
+    // Fibonacci数列 Fib(n)=Fib(n−1)+Fib(n−2)
+    public int fibonacci(int n) {
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        }
+        int first = 0;
+        int second = 1;
+        int index = 2;
+        while (index <= n) {
+            int third = first + second;
+            first = second;
+            second = third;
+            index++;
         }
         return second;
     }
