69f96aa8e96398a294bcf2ad0b93915e1567bbc9
tchen <chen2015tong@gmail.com>
2025-04-25 17:11:55 +0200

    update base iteration with iterator

--- effective-algorithms/src/main/java/iteration/BaseIteration.java
+++ effective-algorithms/src/main/java/iteration/BaseIteration.java
@@ -1,16 +1,36 @@
 package iteration;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class BaseIteration {
 
+    public static void main(String[] args) {
+        List<Integer> values = new ArrayList<>();
+        for (int index = 0; index < 10; index++) {
+            values.add(index);
+        }
+        // TODO. 常规遍历时无法删除数据, 会抛出并发修改异常ConcurrentModificationException
+        for (int value: values) {
+            System.out.println(value);
+            // values.remove(value);
+        }
 
+        // TODO. 使用Iterator迭代器在遍历的同时移除, 直到跳出while循环
+        Iterator<Integer> iterator = values.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+            iterator.remove();
+        }
+        System.out.println(values.size());
+    }
 
 
     // TODO: 在循环的过程中迭代前面计算过的值
     // 第一步有一种可能，
     // 第二步有两种可能，
     // 第三步是前两种情况的和(从第一步和第二步都能够走到第三步)
-    //
-    // O(n) O(1)
     public int climbStairsFib(int n) {
         if (n == 1) {
             return 1;
