9cd90540f07501f59bfdbf671c932766a30e23bb
tchen <chen2015tong@gmail.com>
2025-04-28 17:01:33 +0200

    update max product of 3 numbers

--- data-structure-array/src/main/java/array/MaxProduct3Numbers.java
+++ data-structure-array/src/main/java/array/MaxProduct3Numbers.java
@@ -8,20 +8,64 @@ package array;
 //-1000 <= nums[i] <= 1000
 public class MaxProduct3Numbers {
 
-    // TODO. 只能有0个或2个负数(绝对值最大)
-    // nums = [1,2,3] -> 6
-    // nums = [1,2,3,4] -> 24
-    // nums = [-1,-2,-3] -> -6
+    public static void main(String[] args) {
+        int max1 = Integer.MIN_VALUE;
+        int max2 = Integer.MIN_VALUE;
+        int max3 = Integer.MIN_VALUE;
+        System.out.println(max1 * 2 * 2);
+    }
+
+    // TODO. 只能有0个或2个负数(绝对值最大), 判断是否存在正数
+    // Product = (-Max1)*(-Max2)* MaxValue
+    // Product = (+Max3)*(+Max2)*(+Max1)
+    //
+    // 1,2,3 -> 6
+    // 1,2,3,4 -> 24
+    // -1,-2,-3 -> -6
+    // -1,-2,-3,-4 -> -6
+    // -5 -3 -2 0 1 4 5 -> 75
+    // -100,-98,-1,2,3,4 -> 39200
     //
     // O(n)
     // O(1)
     public int maximumProduct(int[] nums) {
+        if (nums.length == 3) {
+            return nums[0] * nums[1] * nums[2];
+        }
 
+        int maxValue = Integer.MIN_VALUE;
+        int max1 = Integer.MIN_VALUE;
+        int max2 = Integer.MIN_VALUE;
+        int max3 = Integer.MIN_VALUE;
+        int min1 = Integer.MAX_VALUE;
+        int min2 = Integer.MAX_VALUE;
 
-
-
-
-        return 0;
+        for (int num : nums) {
+            maxValue = Math.max(maxValue, num);
+            if (num > 0) {
+                if (num > max1) {
+                    max3 = max2;
+                    max2 = max1;
+                    max1 = num;
+                } else if (num > max2) {
+                    max3 = max2;
+                    max2 = num;
+                } else if (num > max3) {
+                    max3 = num;
+                }
+            } else {
+                if (num < min1) {
+                    min2 = min1;
+                    min1 = num;
+                } else if (num < min2) {
+                    min2 = num;
+                }
+            }
+        }
+        if (max1 == Integer.MIN_VALUE) {
+            return min1 * min2 * maxValue;
+        }
+        return Math.max(min1 * min2 * maxValue, max3 * max2 * max1);
     }
 
 
