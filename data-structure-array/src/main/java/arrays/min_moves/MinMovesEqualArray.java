8477055c67176077cf43d337beaa45f3d9c7567f
chen tong <chen2015tong@gmail.com>
2025-06-12 11:28:54 +0200

    update Minimum Moves to Equal Array question

--- data-structure-array/src/main/java/arrays/min_moves/MinMovesEqualArray.java
+++ data-structure-array/src/main/java/arrays/min_moves/MinMovesEqualArray.java
@@ -5,17 +5,33 @@ package arrays.min_moves;
 // return the minimum number of moves required to make all array elements equal.
 //
 // In one move, you can increment n - 1 elements of the array by 1.
+//
 // n == nums.length
 // 1 <= nums.length <= 10^5
 // -10^9 <= nums[i] <= 10^9
 // The answer is guaranteed to fit in a 32-bit integer.
 public class MinMovesEqualArray {
 
+    // TODO. 使用最小值来配平移动: 最终值 - 初始最小值 = 变化次数
     // nums = [1,2,3] -> 3
     // [1,2,3] => [2,3,3] => [3,4,3] => [4,4,4]
     //
+    // 最终的结果是将最小值2变化到相等值6
+    // nums = [2,3,5] -> 4
+    // [2,3,5] => [3,4,5] => [4,5,5] => [5,6,5] => [6,6,6]
+    //
+    // nums = [1,3,5] -> 6
+    // [1,3,5] => [2,4,5] => [3,5,5] => [4,6,5] => [5,6,6] => [6,7,6] => [7,7,7]
     public int minMoves(int[] nums) {
+        int minValue = nums[0];
+        for (int num : nums) {
+            minValue = Math.min(minValue, num);
+        }
 
-        return 0;
+        int result = 0;
+        for (int num: nums) {
+            result += (num - minValue);
+        }
+        return result;
     }
 }
--- data-structure-array/src/main/java/arrays/min_moves/MinMovesEqualArrayII.java
+++ data-structure-array/src/main/java/arrays/min_moves/MinMovesEqualArrayII.java
@@ -1,23 +1,40 @@
 package arrays.min_moves;
 
+import java.util.Arrays;
+
 // Minimum Moves to Equal Array Elements II
 // Given an integer array nums of size n,
 // return the minimum number of moves required to make all array elements equal.
-// In one move, you can increment or decrement an element of the array by 1.
 // Test cases are designed so that the answer will fit in a 32-bit integer.
+//
+// In one move, you can increment or decrement an element of the array by 1.
 //
 // n == nums.length
 // 1 <= nums.length <= 10^5
 // -10^9 <= nums[i] <= 10^9
 public class MinMovesEqualArrayII {
 
+    // TODO. 将数据往Median中位数进行靠拢
+    //  中位数表示位于数组中50%位置的数据
+    //  平均数(不一定在数组中)不能作为目标值
     // nums = [1,2,3] -> 2
     // [1,2,3] => [2,2,3] => [2,2,2]
     //
+    // nums = [1,3,5] -> 4
+    // [1,3,5] => [2,3,5] => [3,3,5] => [3,3,4] => [3,3,3]
+    //
     // nums = [1,10,2,9] -> 16
     //
+    // nums = [1,0,0,8,6] -> 14
     public int minMoves2(int[] nums) {
+        // 排序后直接取出中位数
+        Arrays.sort(nums);
+        int mid = nums[nums.length / 2];
 
-        return 0;
+        int operations = 0;
+        for (int num: nums) {
+            operations += Math.abs(mid - num);
+        }
+        return operations;
     }
 }
