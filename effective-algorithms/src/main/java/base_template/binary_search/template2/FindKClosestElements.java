e26e35ce8c264ae6ce75c2e88cba1ce2396baa61
chen tong <chen2015tong@gmail.com>
2026-07-08 12:02:11 +0200

    update binary search

--- effective-algorithms/src/main/java/base_template/binary_search/template2/FindKClosestElements.java
+++ effective-algorithms/src/main/java/base_template/binary_search/template2/FindKClosestElements.java
@@ -4,32 +4,37 @@ import java.util.ArrayList;
 import java.util.List;
 
 // Find K Closest Elements
-// Given a sorted integer array arr, two integers k and x, return the k closest integers to x in the array
-// The result should also be sorted in ascending order
+// Given a sorted integer array arr, two integers k and x,
+// return the k closest integers to x in the array
 //
-// |a-x|<|b-x| 或 |a-x|==|b-x| and a<b
-// 两种情况说明a比b更加的接近x, 且x不一定在数组中存在
+// The result should also be sorted in ascending order
+// |a-x|<|b-x| or |a-x|==|b-x| and a<b
+//
+// 1 <= k <= arr.length
+// 1 <= arr.length <= 10^4
+// arr is sorted in ascending order. 有序数据考虑使用二分法查找
+// -10^4 <= arr[i], x <= 10^4
 public class FindKClosestElements {
 
-    // TODO: 判断以二分法搜索的middle为起点的K个相邻区间[mid, mid+k]的两端条件 !!
-    // arr = [1,2,3,4,5,6,8,9,10], k = 4, x = 3
+    // TODO: 二分法搜索middle中心K个相邻区间[mid, mid+k]的条件
+    // [1,2,3,4,5,6,8,9,10], k = 4, x = 3
     // -> [1,2,3,4]   最接近3的4个数，并且有序
     //
     // O(log(n-k)+k)  时间复杂度被划分成两个部分
-    // O(k) 结果列表的空间复杂度
+    // O(k)           结果列表的空间复杂度
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
         int left = 0;
         int right = arr.length - k; // 左边起点的最大位置，给后面的k个位置留下区间
         while (left < right) {
             int mid = left + (right - left) / 2;
-            if (x - arr[mid] > arr[mid + k] - x) { // [mid] ~ [mid + k] 在这区间片段中，如果[mid + k]右边更接近，则把left位置移动上来
-                left = mid + 1;  // 反之如果[mid]左边更加接近，则[mid + k]后面的全部的数据都不用再考虑，则把right降低
+            if (x - arr[mid] > arr[mid + k] - x) { // [mid]-[mid+k]区间片段中
+                left = mid + 1;  // 如果[mid+k]右边更接近，则把left位置移动上来
             } else {
-                right = mid;
+                right = mid;     // 如果[mid]左边更接近，则更新right标记
             }
         }
 
-        // 按照顺序取出k个序列的值 O(k)
+        // 退出条件left==right, 往后取k个序列的值就是结果
         List<Integer> result = new ArrayList<>();
         for (int i = left; i < left + k; i++) {
             result.add(arr[i]);
