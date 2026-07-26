eaa7c7ae0fdc918034cb1749f8e3b6b3deb6b815
chen tong <chen2015tong@gmail.com>
2026-07-26 16:52:42 +0200

    finish bombs bfs and dfs question

--- leetcode-problems/src/main/java/googleTop50/graph/DetonateMaxBombsBFS.java
+++ leetcode-problems/src/main/java/googleTop50/graph/DetonateMaxBombsBFS.java
@@ -1,7 +1,61 @@
 package googleTop50.graph;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 // Detonate the Maximum Bombs
-//
 public class DetonateMaxBombsBFS {
 
+    // TODO. 计算每一个起点能够触发的最大范围
+    //
+    // O(N*N*N)
+    // O(N + N)
+    public int maximumDetonation(int[][] bombs) {
+        int maxCount = 0;
+        for(int index = 0; index < bombs.length; index++) {
+            maxCount = Math.max(maxCount, bfs(bombs, bombs[index], index));
+        }
+        return maxCount;
+    }
+
+    // BFS solution
+    // O(N*N)
+    // O(N + N)
+    private int bfs(int[][] bombs, int[] startBomb, int index) {
+        Queue<int[]> nextBombQueue = new ArrayDeque<>();
+        boolean[] checkedBombs = new boolean[bombs.length]; // N
+
+        // at least invoke one bomb
+        int count = 1;
+        checkedBombs[index] = true;
+
+        // init the next following bombs
+        for(int i = 0; i < bombs.length; i++) {
+            if(!checkedBombs[i] && isConnected(startBomb, bombs[i])) {
+                nextBombQueue.add(bombs[i]);
+                checkedBombs[i] = true;
+                count++;
+            }
+        }
+
+        // continue the BFS loop
+        while(!nextBombQueue.isEmpty()) {
+            int[] nextBomb = nextBombQueue.poll(); // pop from queue
+            for(int i = 0; i < bombs.length; i++) {
+                if(!checkedBombs[i] && isConnected(nextBomb, bombs[i])) {
+                    nextBombQueue.add(bombs[i]);
+                    checkedBombs[i] = true;
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    // 计算距离的平方不需要使用Math.abs(), 考虑Long数值溢出
+    private boolean isConnected(int[] bomb, int[] nextBomb) {
+        long dx = bomb[0] - nextBomb[0];
+        long dy = bomb[1] - nextBomb[1];
+        return dx * dx + dy * dy <= (long) bomb[2] * bomb[2];
+    }
 }
--- leetcode-problems/src/main/java/googleTop50/graph/DetonateMaxBombsDFS.java
+++ leetcode-problems/src/main/java/googleTop50/graph/DetonateMaxBombsDFS.java
@@ -1,6 +1,38 @@
 package googleTop50.graph;
 
 // Detonate the Maximum Bombs
-//
 public class DetonateMaxBombsDFS {
+
+    public int maximumDetonation(int[][] bombs) {
+        int maxCount = 0;
+        for(int index = 0; index < bombs.length; index++) {
+            boolean[] hasChecked = new boolean[bombs.length];
+            int count = dfs(bombs, bombs[index], index, 0, hasChecked);
+            maxCount = Math.max(maxCount, count);
+        }
+        return maxCount;
+    }
+
+    // DFS Solution
+    // 递归时必须把过程的数据带着走，传递给dfs方法参数
+    private int dfs(int[][] bombs, int[] currentBomb, int index, int count, boolean[] hasChecked) {
+        if (hasChecked[index]) {
+            return count;
+        }
+        hasChecked[index] = true;
+        count++;
+
+        for (int i = 0; i < bombs.length; i++) {
+            if (!hasChecked[i] && isConnected(currentBomb, bombs[i])) {
+               count = dfs(bombs, bombs[i], i, count, hasChecked);
+            }
+        }
+        return count;
+    }
+
+    private boolean isConnected(int[] bomb, int[] nextBomb) {
+        long dx = bomb[0] - nextBomb[0];
+        long dy = bomb[1] - nextBomb[1];
+        return dx * dx + dy * dy <= (long) bomb[2] * bomb[2];
+    }
 }
